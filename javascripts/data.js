618f49fcfdd0ea1bc1bb3cb1a0d2515672c5d404
Anastasiia Somchyk <e4nastya@gmail.com>
2018-05-14 20:05:35 -0500

    fist commit

---
+++ javascripts/data.js
@@ -0,0 +1,36 @@
+const domString = require('./dom');
+
+const getAllPups = () => {
+  let pups = [];
+  $.get('../db/pup1.json')
+    .done((data1) => {
+      pups = [...data1.pup1,];
+      $.get('../db/pup2.json')
+        .done((data2) => {
+          pups = [...pups, ...data2.pup2,];
+          $.get('../db/pup3.json')
+            .done((data3) => {
+              pups = [...pups, ...data3.pup3,];
+              domString(pups);
+            })
+            .fail((error3) => {
+              console.error(error3);
+            });
+          // domString(pups);
+        })
+        .fail((error2) => {
+          console.error(error2);
+        });
+    })
+    .fail((error1) => {
+      console.error(error1);
+    });
+};
+
+const initializer = () => {
+  getAllPups();
+};
+
+module.exports = {
+  initializer,
+};
